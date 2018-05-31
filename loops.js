899ddaff77a3ac895d314ad387dc9d5e83bb5a82
Jonathan Hosein <jonathan.hosein@live.com>
2018-05-31 00:25:25 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,30 @@
+function forLoop(array) {
+
+  for (var i = 0; i < 25; i++) {
+    array.push("I am ${i} strange ");
+
+    if (i===1) {
+      array[i] += "loop.";
+    }
+    else {
+      array[i] += "loops."
+    }
+  }
+  return array;
+}
+
+function whileLoop(n) {
+  while(n >= 0){
+    console.log(n);
+    --n;
+  }
+  return "done";
+}
+
+function doWhileLoop(array) {
+  do {
+    array.splice(0,1)
+  } while (array.length === 0 || !maybetrue());
+
+  return array;
+}
